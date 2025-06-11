4a4913bd9bd92aafc77a1e968c06ece15df8a2f4
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-06-11 17:02:49 +0500

    navbar bitdi

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,47 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Advantages from './pages/Advantages.jsx';
+import CreateClubs from './pages/CreateClubs.jsx';
+import Contacts from './pages/Contacts.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/adv",
+        element: <Advantages />
+      },
+      {
+        path: "/create/club",
+        element: <CreateClubs />
+      },
+      {
+        path: "/contacts",
+        element: <Contacts />
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
